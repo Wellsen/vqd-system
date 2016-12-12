4a7f3ef4320e5d30f0a1242c5a91bd4c05eb333b
hujingshuang <jingshuang_hu@163.com>
2016-12-12 22:59:32 +0800

    rename some files

--- MasaicDetection/MasaicDetection.m
+++
@@ -1,8 +0,0 @@
- Image = imread('masaic.bmp');
- Image = rgb2gray(Image);
- [row, col] = size(Image);
- Level = zeros(row, col, 256);
- for i = 0 : 255
-     Level(:, :, i + 1) = Image == i;
-     imshow(Level(:, :, i + 1));
- end
