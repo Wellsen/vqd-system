82c76982fe73098e0c878862e7184f22a832dfa3
hujingshuang <jingshuang_hu@163.com>
2016-12-12 22:54:08 +0800

    code of color cast detection

---
+++ ColorCastDetection/code/ColorCastDetection.m
@@ -0,0 +1,99 @@
+% Ref[1]. COLOR CAST DETECTION FOR SURVEILLANCE VIDEO
+%     USING ADDITIVE AND SUBTRACTIVE COLOR SYSTEM
+
+% Ref[2]. A Two-Stage Blind Image Color Correction
+%     Using Color Cast Estimation
+
+clc;    clear;  close all;
+
+DISPLAY = true;
+
+Files = '../database/';
+Name = 'colorcast_red.bmp';
+
+Image = imread( [Files, Name] );
+
+[M, N, ~] = size(Image);
+
+ImageR = Image( :, :, 1 );    [xR, yR] = imhist( ImageR );
+ImageG = Image( :, :, 2 );    [xG, yG] = imhist( ImageG );
+ImageB = Image( :, :, 3 );    [xB, yB] = imhist( ImageG );
+
+if DISPLAY
+    figure; 
+    plot( yR, xR, 'r-' );  hold on;
+    plot( yG, xG, 'g-' );  hold on;
+    plot( yB, xB, 'b-' );  hold off;
+    legend('R-channel', 'G-channel', 'B-channel');
+    grid on;
+end
+
+% Ref[1]. Formula (1) (2)
+Hmin = min( min( ImageR, ImageG ), ImageB );
+Hmax = max( max( ImageR, ImageG ), ImageB );
+if DISPLAY
+    figure;
+    subplot( 121 );   imshow( Hmin );   title('min');
+    subplot( 122 );   imshow( Hmax );   title('max');
+end
+
+% Ref[1]. Formula (3)
+Ta = 10;
+LbR = ( Hmax - ImageR ) < Ta;
+LbG = ( Hmax - ImageG ) < Ta;
+LbB = ( Hmax - ImageB ) < Ta;
+
+% Ref[1]. Formula (4)
+LdR = ( ImageR - Hmin ) < Ta;
+LdG = ( ImageG - Hmin ) < Ta;
+LdB = ( ImageB - Hmin ) < Ta;
+
+% Li( x, y ) = ( 1, 1, 1 )
+Lb = LbR & LbG & LbB;
+Ld = LdR & LdG & LdB;
+
+% Ref[1]. Formula (5)
+LbR(Lb) = ~LbR( Lb );
+LbB(Lb) = ~LbB( Lb );
+LbG(Lb) = ~LbG( Lb );
+
+LdR(Lb) = ~LdR( Ld );
+LdB(Lb) = ~LdB( Ld );
+LdG(Lb) = ~LdG( Ld );
+
+% Ref[1]. Formula (6)
+length = M * N;
+
+EIRbR = sum( LbR( : ) ) / length;
+EIRbG = sum( LbG( : ) ) / length;
+EIRbB = sum( LbB( : ) ) / length;
+
+EIRdR = sum( LdR( : ) ) / length;
+EIRdG = sum( LdG( : ) ) / length;
+EIRdB = sum( LdB( : ) ) / length;
+
+EIRb = [EIRbR, EIRbG, EIRbB];
+EIRd = [EIRbB, EIRdG, EIRdB];
+
+if DISPLAY
+    figure;
+    bar( [ EIRb ; EIRd ] );
+end
+
+[EIRb, Indexb] = sort( EIRb, 'descend' );
+[EIRd, Indexd] = sort( EIRd, 'descend' );
+
+% Ref[2]. Formula (5)
+T1 = 0.6;   T2 = 0.6;
+CC = zeros( 1, 3 );
+if EIRd( 1, 1 ) - EIRd( 1, 2) > T1
+    CC( 1, Indexd( end ) ) = 1;
+end
+
+if EIRb( 1, 1 ) - EIRb( 1, 2) > T2
+    CC( 1, Indexb( end ) ) = 1;
+end
+% sum(CC)
+Flag = sum(CC) ~= 0
+
+
